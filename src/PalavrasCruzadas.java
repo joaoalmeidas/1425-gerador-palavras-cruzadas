16c5d0265d096e75a87646f30ba1737b7f31f322
joao <joao182cx@gmail.com>
2019-03-01 23:37:19 -0300

    Modifições no método construtor de PalavrasCruzadas.

--- src/PalavrasCruzadas.java
+++ src/PalavrasCruzadas.java
@@ -1,7 +1,74 @@
 
 public class PalavrasCruzadas {
 	
-	private String[][] quadriculado;
-	private String[] dicas;
+	private BancoDeDados banco = new BancoDeDados();
+	private char[][] quadriculado;
+	private String[][] dicas;
+	
+	
+	public PalavrasCruzadas() {
+		
+		banco.conectar();
+		
+		String[][] palavras = banco.selecionaPalavras();
+		String palavraCentral = palavras[0][1];
+		
+		int esquerda = 0, direita = 0;
+		
+		dicas = banco.selecionaDicas(palavras);
+		
+		System.out.println(palavraCentral);
+		
+		for(int i = 0; i < palavraCentral.length(); i++) {
+			
+			System.out.println(palavras[i+1][1]);
+			
+			if(palavras[i+1][1].substring(0, palavras[i+1][1].lastIndexOf(palavraCentral.charAt(i))).length() > esquerda) {
+				
+				esquerda = palavras[i+1][1].substring(0, palavras[i+1][1].lastIndexOf(palavraCentral.charAt(i))).length();
+				
+			}
+			
+			if(palavras[i+1][1].substring(palavras[i+1][1].lastIndexOf(palavraCentral.charAt(i)), palavras[i+1][1].length()).length() > direita) {
+				
+				direita = palavras[i+1][1].substring(palavras[i+1][1].lastIndexOf(palavraCentral.charAt(i)), palavras[i+1][1].length()).length();
+				
+			}
+				
+		}
+		
+		quadriculado = new char[palavraCentral.length()][esquerda + direita];
+		
+		System.out.println(esquerda);
+		
+		for(int i = 0; i < palavraCentral.length(); i++) {
+			
+			quadriculado[i][esquerda] = palavraCentral.charAt(i);
+			
+		}
+		
+		for(int i = 1; i < palavras.length; i++) {
+			
+			
+			
+		}
+		
+		for(int i = 0; i < quadriculado.length; i++) {
+			
+			for(int j = 0; j < quadriculado[0].length; j++) {
+				
+				System.out.printf("%c", quadriculado[i][j]);
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+		banco.desconectar();
+
+	}
+	
+	
 	
 }
--- src/Teste.java
+++ src/Teste.java
@@ -3,37 +3,9 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-		BancoDeDados banco = new BancoDeDados();
-		String[][] palavras;
-		String[][] dicas;
+		PalavrasCruzadas palavras = new PalavrasCruzadas();
 		
-		banco.conectar();
 		
-		if(banco.estaConectado()) {
-			
-			palavras = banco.selecionaPalavras();
-			dicas = banco.selecionaDicas(palavras);
-			
-			for(int i = 0; i < palavras.length; i++) {
-				
-				for(int j = 0; j < palavras[0].length; j++) {
-					
-					System.out.printf("%s ", palavras[i][j]);
-					System.out.printf("%s ", dicas[i][j]);
-					
-				}
-				
-				System.out.println();
-				
-			}
-			
-			banco.desconectar();
-			
-		}else {
-			
-			System.out.println("Não foi possível conectar com o banco.");
-			
-		}
 		
 	}
 
