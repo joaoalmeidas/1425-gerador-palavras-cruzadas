87c82dbeb45477aeff9cc3990d12bf8e2427c75e
joao <joao182cx@gmail.com>
2019-03-03 21:40:18 -0300

    Criação da classe PalavrasCruzadasPanel e criação dos getters e setters da classe PalavrasCruzadas.

---
+++ src/PalavrasCruzadasPanel.java
@@ -0,0 +1,10 @@
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PalavrasCruzadasPanel extends JPanel {
+
+	private PalavrasCruzadas palavraCruzada;
+	private JTextField[][] quadriculado;
+	
+	
+}
