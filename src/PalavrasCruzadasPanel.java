a36d7d1541fba6f4a068d349ab1c2e29e2842cd7
joao <joao182cx@gmail.com>
2019-03-05 13:31:38 -0300

    Modificação da fonte dos JTextFields do jogo.

--- src/PalavrasCruzadasPanel.java
+++ src/PalavrasCruzadasPanel.java
@@ -1,6 +1,9 @@
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -52,6 +55,10 @@ public class PalavrasCruzadasPanel extends JPanel {
 						|| palavraCruzada.getQuadriculado()[i][j] == '-') {
 					
 					quadriculado[i][j] = new JTextField(1);
+					quadriculado[i][j].setFont(new Font("Arial", Font.BOLD, 45));
+					
+					
+					
 					add(quadriculado[i][j]);
 					
 				}else {
